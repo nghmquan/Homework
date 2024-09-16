c6814859d021cf61958d393ef6f472c61c547a48
Nguyễn Hoàng Minh Quân <109788332+nghmquan@users.noreply.github.com>
2024-09-16 18:57:18 +0700

    Update homework

--- Assets/Homeworks/Homework3.cs
+++ Assets/Homeworks/Homework3.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class Homework3 : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer currentObject;
-    [SerializeField] private Transform objectPosition; //Variable for position of object
+    [SerializeField] private SpriteRenderer currentSpriteObject;
+    [SerializeField] private Transform currentObjectPosition; //Variable for position of object
     [SerializeField] private float moveSpeed = 5; // Variable for speed of object
     [SerializeField] private GameObject[] arrayObject;
 
@@ -31,75 +31,79 @@ public class Homework3 : MonoBehaviour
             Vector3 mouseClick = Input.mousePosition; // Variable get the mouse's position in screen space
             mouseClick = Camera.main.ScreenToWorldPoint(mouseClick); // Change mouse position from screen space to world postition
             mouseClick.z = 10; // Set Z value = 10 to not dissapear in world space
-            transform.position = Vector3.MoveTowards(objectPosition.position,
+            transform.position = Vector3.MoveTowards(currentObjectPosition.position,
                 mouseClick, moveSpeed * Time.deltaTime); // Move object from object position to mouse click position with speed of object
         }
     }
 
+    // Function for set color for current object
     private ColorType SetColorOfObject()
     {
         switch (currentObjectColor)
         {
             case ColorType.White:
-                currentObject.color = Color.white;
+                currentSpriteObject.color = Color.white;
                 break;
             case ColorType.Yellow:
-                currentObject.color = Color.yellow;
+                currentSpriteObject.color = Color.yellow;
                 break;
             case ColorType.Green:
-                currentObject.color = Color.green;
+                currentSpriteObject.color = Color.green;
                 break;
             case ColorType.Blue:
-                currentObject.color = Color.blue;
+                currentSpriteObject.color = Color.blue;
                 break;
             case ColorType.Orange:
-                currentObject.color = new Color(1f, 0.5f, 0f);
+                currentSpriteObject.color = new Color(1f, 0.5f, 0f);
                 break;
             case ColorType.Red:
-                currentObject.color = Color.red;
+                currentSpriteObject.color = Color.red;
                 break;
         }
         return currentObjectColor;
 
     }
 
+    // Function for collision handling
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Color"))
+        if (collision.gameObject.CompareTag("Color")) // If object collided object with tag "Color"
         {
-            for (int i = 0; i < arrayObject.Length; i++)
-            {
-                if (currentObjectColor == ColorType.Red)
-                {
-                    return;
-                }
 
-                CombineObjectColor();
-                Destroy(arrayObject[i]);
+            if (currentObjectColor == ColorType.Red)
+            {
+                Debug.Log("Color red can not merc");
+                return;
             }
+
+            if (arrayObject[(int)currentObjectColor].gameObject == collision.gameObject)
+            {
+                CombineObjectColor();
+                Destroy(collision.gameObject);
+            }
+            
+
         }
     }
 
     private void CombineObjectColor()
     {
-        for(int i = 0; i < arrayObject.Length; i++)
+        if (currentObjectColor < ColorType.Red)
         {
-            if(currentObjectColor < ColorType.Red)
-            {
-                currentObjectColor++;
-                SetColorOfObject();
-                Debug.Log(currentObjectColor);
-            }
+            currentObjectColor++;
+            SetColorOfObject();
+            Debug.Log(currentObjectColor);
         }
+
     }
 }
 
 public enum ColorType
 {
-    White,
-    Yellow,
-    Green,
-    Blue,
-    Orange,
-    Red
+    White = 0,
+    Yellow = 1,
+    Green = 2,
+    Blue = 3,
+    Orange = 4,
+    Red = 5
 }
--- Assets/Homeworks/Homework4.cs
+++ Assets/Homeworks/Homework4.cs
@@ -1,28 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Homework4 : MonoBehaviour
 {
-    [SerializeField] private Transform objectMove;
-    [SerializeField] private GameObject wallObject;
-    [SerializeField] private LayerMask wallLayer;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform objectMove; // Variable for object moving
+    [SerializeField] private GameObject wallObject; // Variable forwall object
+    [SerializeField] private SpriteRenderer spriteObject; // Variable for sprite object
+    
+    [SerializeField] private LayerMask wallLayer; // Variable for layer of wall
+
+    [SerializeField] private float moveSpeed = 5; // Variable for speed when object moving
+    [SerializeField] private float rayHitNumber = 5; // Variable for display ray cast
+
+    private Vector3 direction; // Variable for direction
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector2.right; // Initialize the right direction of Vector2
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Debug.DrawRay(transform.position, direction, Color.red, 0.1f); // Show the the red line in display game scene
+        Move(); // Run function to object move
     }
 
     private void Move()
     {
-        transform.position = Vector3.MoveTowards(objectMove.position, wallObject.transform.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(objectMove.position, objectMove.position + direction, moveSpeed * Time.deltaTime);
+
+        if (CheckWallLayer() == true)
+        {
+            spriteObject.flipY = !spriteObject.flipY;
+            direction = -direction;
+        }
+
     }
+
+    private bool CheckWallLayer()
+    {
+        RaycastHit2D rayHit = Physics2D.Raycast(objectMove.position, direction, rayHitNumber, wallLayer);
+        return rayHit.collider != null;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Wall"))
+    //    {
+    //        spriteObject.flipY = !spriteObject.flipY;
+    //        direction = -direction;
+    //    }
+    //}
 }
